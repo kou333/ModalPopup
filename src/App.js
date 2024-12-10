f64a62e55188662f48c486e432dc2fb8f73bd5b5
kou333 <naikkoustubh@gmail.com>
2024-12-10 17:25:13 +0530

    Custom model popup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ModalTest from './components/custom-model-popup/modal-test';
+
+function App() {
+  return (
+    <div className="App">
+      <ModalTest/>
+    </div>
+  );
+}
+
+export default App;
